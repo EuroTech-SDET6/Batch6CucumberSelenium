0e5863b2d29e40c66340c8436a6e7ad5f5b41d64
ustazeynep <zeynep.n.usta@outlook.com>
2023-04-23 19:30:44 +0100

    Adding screenshot method into Hooks class

--- src/test/java/com/eurotech/step_definitions/Hooks.java
+++ src/test/java/com/eurotech/step_definitions/Hooks.java
@@ -3,6 +3,9 @@ package com.eurotech.step_definitions;
 import com.eurotech.utilities.Driver;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
@@ -17,8 +20,11 @@ public class Hooks {
     }
 
     @After
-    public void tearDown(){
-
+    public void tearDown(Scenario scenario){
+        if(scenario.isFailed()){
+            final byte[] screenshot = ((TakesScreenshot) Driver.get()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot,"image/png","screenshot");
+        }
         Driver.closeDriver();
     }
 
