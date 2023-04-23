8d73f313cd933bb2824d5ffe905652fd10d08d90
ustazeynep <zeynep.n.usta@outlook.com>
2023-04-23 19:29:57 +0100

    Adding basic html report plugin

--- src/test/java/com/eurotech/runners/CukesRunner.java
+++ src/test/java/com/eurotech/runners/CukesRunner.java
@@ -7,7 +7,8 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {"json:target/cucumber.json"},
+        plugin = {"json:target/cucumber.json",
+                "html:target/default-html-reports.html"},
         features = "src/test/resources/Features",
         glue = "com/eurotech/step_definitions",
         dryRun = false,
